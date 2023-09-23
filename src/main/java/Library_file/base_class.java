6a5f0ad5fe83cf88e8b01cc721a8f0718bbe0fc4
Sandhyachumbhale <shailachumbhale@gmail.com>
2023-09-23 12:08:05 +0530

    first commit

---
+++ src/main/java/Library_file/base_class.java
@@ -0,0 +1,31 @@
+package Library_file;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.*;
+
+public class base_class {
+	
+	public WebDriver driver;
+	
+	public void initilizebrowser()
+	
+	{
+		ChromeOptions op=new ChromeOptions();
+		op.addArguments("--remote-allow-Origins=*","iqnore-certificate-errors");
+		
+		driver=new ChromeDriver(op);
+		
+		driver.get("http://adactinhotelapp.com");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		
+	}
+	
+
+}
